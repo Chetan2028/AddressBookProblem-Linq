fecc46aed0fda98fa9283e458801e248d220a80d
Chetan2028 <bmchetan2028@gmail.com>
2020-11-27 21:33:23 +0530

    Add project files.

---
+++ AddressBookLinq/AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookLinq
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
